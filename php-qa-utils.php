6140905980d3c7758e964c3e6b09b8fca07238b2
k-yamamoto-balocco <k.yamamoto@balocco.info>
2023-06-29 16:50:04 +0900

    品質管理関連追加

---
+++ php-qa-utils.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * php-qa-utils 設定ファイル
+ * パスは、プロジェクトルートからの相対パスで指定する。
+ */
+return [
+    'path' => [
+        //シェルスクリプト格納先
+        'scripts' => 'qa/scripts',
+        //実行結果格納ディレクトリ
+        'results' => 'qa/results',
+        //設定ファイル格納先
+        'settings' => 'qa/settings'
+    ],
+];
