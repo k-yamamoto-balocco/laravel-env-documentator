6d435d50659c382c476822fed6491d8f56f22e87
k-yamamoto-balocco <k.yamamoto@balocco.info>
2023-07-05 17:18:33 +0900

    Validator追加

--- src/Config/Validator/Items/DestinationValidator.php
+++ src/Config/Validator/Items/DestinationValidator.php
@@ -8,14 +8,16 @@ use GitBalocco\LaravelEnvDocumentator\Config\Validator\ValidatorInterface;
 use Respect\Validation\Validatable;
 use Respect\Validation\Validator as v;
 
-class CipherValidator extends BaseValidator implements ValidatorInterface
+class DestinationValidator extends BaseValidator implements ValidatorInterface
 {
-    /**
-     * @return Validatable
-     * @author kenji yamamoto <k.yamamoto@balocco.info>
-     */
     public function definition(): Validatable
     {
-        return v::in(['aes-128-cbc', 'aes-256-cbc', 'aes-128-gcm', 'aes-256-gcm']);
+        return v::notEmpty()
+            ->arrayVal()
+            ->call('count', v::lessThan(8))
+            ->each(
+                v::notEmpty()->stringType()
+            );
     }
+
 }
